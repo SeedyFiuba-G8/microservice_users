581b5684f364b77f9398adb2fd20f841276561be
Nicolas Aguerre <naguerre@fi.uba.ar>
2021-05-05 17:50:38 -0300

    Change db connection method

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,25 @@ import pg from 'pg';
 
 const app = express();
 
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
 app.get('/', (req, res) => {
     res.send('Users microservice');
 });
 
 const port = process.env.PORT || config.port;
 
-pg.connect(process.env.DATABASE_URL, (err, client, done) => {
-    client.query('SELECT * FROM Users', function (err, result) {
-        done();
-        if (err) return console.error(err);
-        console.log(result.rows);
-    });
+client.query('SELECT * FROM Users', (err, res) => {
+    if (err) throw err;
+    for (const row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
 });
 
 app.listen(port, () => {
