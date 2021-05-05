2c2832e6abda99c31fe405afed326b7015e562db
Nicolas Aguerre <naguerre@fi.uba.ar>
2021-05-05 17:17:57 -0300

    DB Connection dev test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from './const/config';
+import pg from 'pg';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || config.port;
+
+pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    client.query('SELECT * FROM Users', function (err, result) {
+        done();
+        if (err) return console.error(err);
+        console.log(result.rows);
+    });
+});
 
 app.listen(port, () => {
     console.log('The application is listening on port 3000!');
