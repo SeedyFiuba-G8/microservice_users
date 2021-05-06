4fd512ac84ba8533238e0fe7632ec020b391b506
Nicolas Aguerre <naguerre@fi.uba.ar>
2021-05-06 16:55:51 -0300

    Test database connection fixed

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,18 @@ const client = new pg.Client({
     },
 });
 */
-
-const client = new pg.Client({
+const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
+});
+client.connect((err) => {
+    if (err) {
+        console.error('connection error', err.stack);
+    } else {
+        console.log('connected');
+    }
 });
 
 app.get('/', (req, res) => {
