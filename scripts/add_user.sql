64153921c1e4a03dfd8c4213a204ba3d2d80a11b
Mauro Parafati <mparafati@fi.uba.ar>
2021-05-09 20:57:46 -0300

    [ADD] Some SQL scripts

---
+++ scripts/add_user.sql
@@ -0,0 +1,11 @@
+-- Add user example
+
+INSERT INTO public.users(
+	email, first_name, last_name, pass, profile_pic_url)
+	VALUES (
+		'memis@pomofot.ar',
+		'memis',
+		'pomofot',
+		'memis123pomo321fot',
+		'imgur.img/jsdkf.png'
+	);
