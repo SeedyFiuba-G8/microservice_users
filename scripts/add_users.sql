ee88c725cb80854de277b3891f0d73a6609553c5
Mauro Parafati <mparafati@fi.uba.ar>
2021-05-13 15:55:42 -0300

    [MOD] Scripts

--- scripts/add_users.sql
+++ scripts/add_users.sql
@@ -2,10 +2,18 @@
 
 INSERT INTO public.users(
 	email, first_name, last_name, pass, profile_pic_url)
-	VALUES (
+	VALUES
+	(
 		'memis@pomofot.ar',
 		'memis',
 		'pomofot',
 		'memis123pomo321fot',
 		'imgur.img/jsdkf.png'
+	),
+	(
+		'facu@arana.com',
+		'facundo',
+		'arana',
+		'unapassword',
+		'imgur.img/hola.png'
 	);
