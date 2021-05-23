a5b2d2ede29332230db70728dddf6ed69ec27315
Mauro Parafati <mparafati@fi.uba.ar>
2021-05-23 03:47:20 -0300

    [MOD] Added default value to init_db script

--- init_db.sh
+++ init_db.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
+DEFAULT_URL='postgres://postgres:postgres@localhost:3001/postgres'
+: "${DATABASE_URL:=$DEFAULT_URL}"
+
 cd ./scripts
 echo "Seedy FIUBA - Users microservice\n"
 
 echo "Creating database..."
-
 psql $DATABASE_URL -f create_db.sql
 echo "Database created!\n"
 
