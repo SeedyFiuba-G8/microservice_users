44a7b47bd3e5b28ef4c7318a22df331c41ed02bb
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-12 20:28:38 -0300

    [MOD] Users and admins initial data

--- scripts/add_admins.sql
+++ scripts/add_admins.sql
@@ -1,15 +1,8 @@
 -- Add admin example
-
-INSERT INTO public.admins
-    (id, email, password)
-	VALUES
+INSERT INTO public.admins (id, email, password)
+VALUES
 	(
-		'123e4567-e89b-12d3-a456-428614174000',
-		'admin@gmail.com',
-		'$2b$10$kSakto1QTzIr.WTIQ2ieaOdJRD/l8oq.lcp9Z3Y..IA6ZGuHcAT/u'
-	),
-	(
-		'9bb37345-41ad-471e-adc3-989fd05e5b6f',
-		'facu@arana.com',
-		'$2b$10$kSakto1QTzIr.WTIQk8eaOdJRD/l8oq.lcp9Z3Y..IA6ZGuHcAT/u'
+		'f84683c1-2d00-47a6-9335-df0dd2718aee',
+		'admin@admin.com',
+		'$2b$10$Vg1ZosjDBfxF4zfHly2QjuLh2AX/tqOOXLtHOxy8eJGzhG29pFkXy'
 	);
--- scripts/add_users.sql
+++ scripts/add_users.sql
@@ -1,21 +1,12 @@
 -- Add user example
 
 INSERT INTO public.users
-	(id, email, password, first_name, last_name, profile_pic_url)
+	(id, email, password, first_name, last_name)
 	VALUES
 	(
-		'123e4567-e89b-12d3-a456-426614174000',
-		'memis@pomofot.ar',
-		'$2b$10$kSakto1QTzIr.WTIQ2ieaOdJRD/l8oq.lcp9Z3Y..IA6ZGuHcAT/u',
-		'memis',
-		'pomofot',
-		'imgur.img/jsdkf.png'
-	),
-	(
-		'9bb37345-41ad-471e-adc3-980fd05e5b6f',
-		'facu@arana.com',
-		'$2b$10$kSakto1QTzIr.WTIQk8eaOdJRD/l8oq.lcp9Z3Y..IA6ZGuHcAT/u',
-		'facundo',
-		'arana',
-		'imgur.img/hola.png'
+		'ca718a21-a126-484f-bc50-145126a6f75b',
+		'user@user.com',
+		'$2b$10$R3YT8/4SxWGfajHg6lSJ3eLw2ASxqeO8kOhtN2t3h2RzBUln3YjEa',
+		'user',
+		'user'
 	);
