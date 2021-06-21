41494f07ae3207b0377708ca0a24d3e5625252a2
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-21 05:01:17 -0300

    [ADD] TO-DO unit testing

---
+++ test/unit/repositories/adminRepository.test.js
@@ -0,0 +1,13 @@
+// const containerFactory = require('../../testContainerFactory');
+
+// const container = containerFactory.createContainer();
+
+describe('adminRepository', () => {
+  //   let adminRepository;
+
+  //   beforeEach(() => {
+  //     adminRepository = container.get('adminRepository');
+  //   });
+
+  it('TO-DO', () => expect(true).toBeTruthy());
+});
---
+++ test/unit/repositories/userRepository.test.js
@@ -0,0 +1,13 @@
+// const containerFactory = require('../../testContainerFactory');
+
+// const container = containerFactory.createContainer();
+
+describe('userRepository', () => {
+  //   let userRepository;
+
+  //   beforeEach(() => {
+  //     userRepository = container.get('userRepository');
+  //   });
+
+  it('TO-DO', () => expect(true).toBeTruthy());
+});
