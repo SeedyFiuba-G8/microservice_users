3061864af653db5b858d8643fb1051ea4eaf35de
Mauro Parafati <mparafati@fi.uba.ar>
2021-08-01 23:00:24 -0300

    Dump apikeys

--- config/default.js
+++ config/default.js
@@ -94,6 +94,7 @@ module.exports = {
   monitoring: true,
   services: {
     apikeys: {
+      enabled: true,
       header: 'x-api-key',
       baseUrl: 'https://sf-tdp2-apikeys-main.herokuapp.com/',
       key: {
